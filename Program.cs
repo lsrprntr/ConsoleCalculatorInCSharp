e495c9a27dd93482b244689330ad081cc9e4029e
Sams Sutio <39038103+lsrprntr@users.noreply.github.com>
2024-02-16 13:12:22 +1100

    c

--- Program.cs
+++ Program.cs
@@ -20,41 +20,49 @@ namespace ConsoleCalculator
                 Console.WriteLine("Calculator");
                 Console.WriteLine("----------");
 
-                Console.Write("Enter number 1: ");
-                num1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter number 2: ");
-                num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Enter an option: ");
-                Console.WriteLine("\t+ : Add");
-                Console.WriteLine("\t- : Subtract");
-                Console.WriteLine("\t* : Multiply");
-                Console.WriteLine("\t/ : Divide");
-
-                switch (Console.ReadLine())
+                try
                 {
-                    case "+":
-                        result = num1 + num2;
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        break;
-                    case "/":
-                        result = num1 / num2;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Option");
-                        result = 0;
-                        break;
+                    Console.Write("Enter number 1: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter number 2: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+
+                    // Case options
+                    Console.WriteLine("Enter an option: ");
+                    Console.WriteLine("\t+ : Add");
+                    Console.WriteLine("\t- : Subtract");
+                    Console.WriteLine("\t* : Multiply");
+                    Console.WriteLine("\t/ : Divide");
+                    switch (Console.ReadLine())
+                    {
+                        case "+":
+                            result = num1 + num2;
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            break;
+                        case "/":
+                            result = num1 / num2;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Option");
+                            result = 0;
+                            break;
+                    }
+
+                    Console.WriteLine($"Your result: {result}");
+
+                    Console.WriteLine("Would you like to continue? (Y/N)");
                 }
-
-                Console.WriteLine($"Your result: {result}");
-
-                Console.WriteLine("Would you like to continue? (Y/N)");
+                catch (Exception e) // Catch wrong inputs
+                {
+                    Console.WriteLine($"Error: {e}");
+                    Console.WriteLine("Would you like to retry? (Y/N)");
+                }
 
 
             } while (Console.ReadLine().ToUpper() == "Y");
